4e163091b8b28371488fded0f0b55e1ba0930113
Lucas S. Guimarães <lucasasafe@outlook.com.br>
2019-03-26 13:49:39 -0300

    Mocking de dados no seeds
    Para testes depois de dropar o banco de dados

--- Gemfile
+++ Gemfile
@@ -5,6 +5,8 @@ ruby '2.6.2'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.2', '>= 5.2.2.1'
+# A library for generating fake data such as names, addresses, and phone numbers.
+gem 'faker', '~> 1.6', '>= 1.6.6'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3', '< 1.4'
 # Use Puma as the app server
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Gerando os tipos de contato (Kinds)...'
+Kind.create!([{ description: 'Amigo' },
+              { description: 'Contato' },
+              { description: 'Comercial' }])
+puts 'Gerando os tipos de contato (Kinds)... [OK]'
+
+puts 'Gerando os contatos (Contacts)...'
+10.times do |i|
+  Contact.create!(nome: Faker::Name.name,
+                  email: Faker::Internet.email,
+                  kind: Kind.all.sample,
+                  rmk: Faker::Lorem.paragraph([1, 2, 3, 4, 5].sample))
+end
+puts 'Gerando os contatos (Contacts)... [OK]'
